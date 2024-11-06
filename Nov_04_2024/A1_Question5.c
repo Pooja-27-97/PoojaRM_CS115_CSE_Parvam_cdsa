5561c38f8522337976992114441bdaf9508185f0
Pooja R M <poojarm2797@gmail.com>
2024-11-06 10:41:32 -0500

    Assignment Questions

---
+++ Nov_04_2024/A1_Question5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    float num1;
+    scanf("%f",&num1);
+
+    int num2 = num1;
+
+    printf("Floating point %.2f\n",num1);
+    printf("Integer point %d",num2);
+
+    return 0;
+}
