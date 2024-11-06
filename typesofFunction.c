bde7ebdcfa4eca7f2092f3e1c30ce9d209592b2c
Pooja R M <poojarm2797@gmail.com>
2024-11-06 03:44:30 -0500

    Update typesofFunction.c

--- typesofFunction.c
+++ typesofFunction.c
@@ -2,10 +2,10 @@
 
 void add() {//fn without argument and without return type
     int a=3,b=4;
-    printf("%d",a+b);
+    printf("%d\n",a+b);
 }
 void addArg(int a, int b) {//fn with argument and without return type
-    printf("%d",a+b);
+    printf("%d\n",a+b);
 }
 int addWithoutArg(){//fn without argument and with return type(data type of the stored data/value)
     int a=9 , b=10;
@@ -18,7 +18,7 @@ int main() {
     add();
     addArg(1,2);
     printf("%d\n", addWithoutArg());
-    printf("%d",addWithArg(20,10));
+    printf("%d\n",addWithArg(20,10));
 
     return 0;
 }
